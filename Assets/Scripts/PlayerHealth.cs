db3c65dc57d5f72b7c34a667c943326fe7e1148a
nathanael <nathanael.vilain@gmail.com>
2021-06-26 15:50:15 -0400

    ajout possibilité gagner de la vie

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -13,6 +13,19 @@ public class PlayerHealth : MonoBehaviour
     public SpriteRenderer graphics;
     public HealthBar healthBar;
 
+    public static PlayerHealth instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de PlayerHealth dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+
     void Start()
     {
         currentHealth = maxHealth;
@@ -27,6 +40,19 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    public void HealPlayer(int amount)
+    {
+        if((currentHealth + amount) > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            currentHealth += amount;
+        }
+
+        healthBar.SetHealth(currentHealth);
+    }
     public void TakeDamage(int damage)
     {
         if(!isInvicible)
