63bae712c2653bfebd17c230ad5055106387fab3
nathanael <nathanael.vilain@gmail.com>
2021-06-27 22:33:25 -0400

    Ajout dialogue + PNJ

---
+++ Assets/Scripts/LoadAndSaveData.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class LoadAndSaveData : MonoBehaviour
+{
+    public static LoadAndSaveData instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de LoadAndSaveData dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+
+    void Start()
+    {
+        Inventory.instance.coinsCount = PlayerPrefs.GetInt("coinsCount", 0);
+        Inventory.instance.UpdateTextUI();
+
+       // int currentHealth = PlayerPrefs.GetInt("playerHealth", PlayerHealth.instance.maxHealth);
+       // PlayerHealth.instance.currentHealth = currentHealth;
+       // PlayerHealth.instance.healthBar.SetHealth(currentHealth);
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetInt("coinsCount", Inventory.instance.coinsCount);
+        
+        if(CurrentSceneManager.instance.levelToUnlock > PlayerPrefs.GetInt("levelReached", 1))
+        {
+            PlayerPrefs.SetInt("levelReached", CurrentSceneManager.instance.levelToUnlock);
+        }
+        //PlayerPrefs.SetInt("playerHealth", PlayerHealth.instance.currentHealth);
+    }
+}
